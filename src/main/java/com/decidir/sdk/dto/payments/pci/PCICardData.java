ae85c5471245d29b862bd3cf4f01ba35008a1ca9
Bartolome Albarracin Guareschi <122897005+BartoDev@users.noreply.github.com>
2023-04-12 15:22:51 -0300

    feat(CardTokenData): [FPP11-5652] more fields are added. feat(FraudDetectionPciModel): [FPP11-5652] class added. feat(installmentSpv): [FPP11-5652] class added. feat(Payment): [FPP11-5652]  Description field are added. feat(PaymentPciRequest): [FPP11-5652] more fields are added. feat(PaymentRequest): [FP11-5352] Spv object are added. feat(PaymentResponse): [FPP11-5652] more fields are added. feat(PCICardData): [FPP11-5652] more fields are added. feat(Spv): [FPP11-5652] Class added.

--- src/main/java/com/decidir/sdk/dto/payments/pci/PCICardData.java
+++ src/main/java/com/decidir/sdk/dto/payments/pci/PCICardData.java
@@ -10,9 +10,18 @@ import java.io.Serializable;
 public abstract class PCICardData implements Serializable {
 
 	private String security_code;
+	private String last_four_digits;
+	private String card_holder_birthday;
+	private Integer card_holder_door_number;
 	private CardFraudDetectionData fraud_detection;
 	private String ip_address;
-	
+
+	public String getLast_four_digits() {return last_four_digits;}
+	public void setLast_four_digits(String last_four_digits) {this.last_four_digits = last_four_digits;}
+	public String getCard_holder_birthday() {return card_holder_birthday;}
+	public void setCard_holder_birthday(String card_holder_birthday) {this.card_holder_birthday = card_holder_birthday;}
+	public Integer getCard_holder_door_number() {return card_holder_door_number;}
+	public void setCard_holder_door_number(Integer card_holder_door_number) {this.card_holder_door_number = card_holder_door_number;}
 	public String getSecurity_code() {
 		return security_code;
 	}
@@ -31,5 +40,4 @@ public abstract class PCICardData implements Serializable {
 	public void setIp_address(String ip_address) {
 		this.ip_address = ip_address;
 	}
-	
 }
