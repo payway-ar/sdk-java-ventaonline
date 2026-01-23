45f8d4cfa45da4cf0193daa0670640c18c32d630
Barbara <58581803+Bcolmenares@users.noreply.github.com>
2026-01-23 12:19:57 -0300

    Update CheckoutApi.java

--- src/main/java/com/decidir/sdk/resources/CheckoutApi.java
+++ src/main/java/com/decidir/sdk/resources/CheckoutApi.java
@@ -9,7 +9,7 @@ import retrofit2.http.POST;
 
 public interface CheckoutApi {
 	
-	@POST("payments/link")
+	@POST("/link")
 	Call <CheckoutResponse> checkoutHash (@Body CheckoutRequest checkoutReq);
 
 }
